903272f49426585a780eb84dc2ea413e3bdcc611
Serbe <serbenv@gmail.com>
2023-01-20 16:11:06 +0300

    1

--- nodes/terneo.js
+++ nodes/terneo.js
@@ -1,7 +1,19 @@
-import got from 'got';
+import fetch from "node-fetch"
 
-const { data } = await got.post('http://192.168.15.82/api.cgi', {
-    json: { "cmd": 4 }
-}).json();
+const data = {
+  cmd: 4,
+  sn: "4A002C000150535658313720000159",
+}
 
-console.log(data);
+const response = await fetch("http://127.0.0.1:8882/api.cgi", {
+  method: "post",
+  body: JSON.stringify(data),
+})
+
+console.log(response.ok)
+console.log(response.status)
+console.log(response.statusText)
+console.log(response.headers.raw())
+console.log(response.headers.get("content-type"))
+const body = await response.json()
+console.log(body)
